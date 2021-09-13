15a0331e93fd5574d4db418efe99531e5585f6aa
Sibints2000 <sibints2000@gmail.com>
2021-09-13 16:48:38 +0530

    FIfth Commit

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ const notion = new Client({
     auth: process.env.NOTION_TOKEN
 })
 
-
+const listDatabases =async () => {
+    const res = await notion.databases.list()
+    console.log(res);
+}
+listDatabases()
