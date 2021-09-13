6f1cf71b823aebef2e22230e6c97d1f47dbff841
Sibints2000 <sibints2000@gmail.com>
2021-09-13 17:26:38 +0530

    Twenty Sixth Commit

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.get('/videoes', async(req, res) => {
-    const videos 
+app.get('/videos', async(req, res) => {
+    const videos = await getVideos()
+    res.json(videos)
 })
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
