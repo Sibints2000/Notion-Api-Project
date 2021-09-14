06dbbca738f0721a7a9bb1d017f5e88e7715287b
Sibints2000 <sibints2000@gmail.com>
2021-09-14 18:18:08 +0530

    Twentieth Commit

--- public/js/main.js
+++ public/js/main.js
@@ -5,4 +5,14 @@ let loading = false
 const getVideosFromBackend = async () => {
     loading = true
     const res = await fetch('hhtp://localhost:5000/videos')
+    const data = await res.json()
+    loading = false 
+    return data
 }
+
+const addVideosToDom = async () => {
+    const videos = await getVideosFromBackend()
+    console.log(videos);
+}
+
+addVideosToDom()
