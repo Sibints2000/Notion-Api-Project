42659db398e49e1acb1ea8f4db5775ed186bc97f
Sibints2000 <sibints2000@gmail.com>
2021-09-14 18:11:51 +0530

    Eighteenth Commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,8 @@
 const videosEl = document.querySelector('#videos');
 const loadingEl = document.querySelector('#loading');
+let loading = false
+
+const getVideosFromBackend = async () => {
+    loading = true
+    const res = await fetch('hhtp://localhost:5000/videos')
+}
